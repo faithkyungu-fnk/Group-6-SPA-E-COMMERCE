5b1495cab1eeda1fc4b450e27a033d240db486dd
levisnelson54-wq <levisnelson54@gmail.com>
2026-08-16 23:17:54 +0300

    added some pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+cat > src/App.jsx <<'EOF'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import AddCoffee from "./pages/AddCoffee";
+import db from "./API/db";
 
 function App() {
+  const [coffees, setCoffees] = useState(db.coffees);
+
   return (
     <>
       <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/menu"
+          element={<Menu coffeeItems={coffees} />}
+        />
+
+        <Route
+          path="/coffee/new"
+          element={<AddCoffee />}
+        />
+
+        <Route
+          path="/coffee/:coffeeId"
+          element={<div>Coffee details coming soon.</div>}
+        />
+      </Routes>
     </>
   );
 }
 
 export default App;
+EOF
