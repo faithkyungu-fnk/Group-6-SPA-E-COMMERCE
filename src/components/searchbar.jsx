50acf6e8254aa23136e7ea3fbc1a591472104462
EmmanuelMwangi021 <manumwangi2@gmail.com>
2026-08-17 09:13:46 +0300

    Fixed the searchBar functionality and added some styling

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,15 +1,28 @@
 
 function SearchBar({searchTerm, onSearchChange}) {
     return (
-        <div className= "search-bar">
-            <input
-                type="text"
-                placeholder= "search for a coffee..."
-                value= {searchTerm}
-                onChange= {(e) => onSearchChange(e.target.value)}
-                />
-                </div>
+        <form
+        className="search-bar"
+        onSubmit={(event) => event.preventDefault()}
+        >
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(event) => onSearchChange(event.target.value)}
+          placeholder="Search coffee..."
+          aria-label="Search coffee"
+        />
+
+        <button
+          className="button button-dark"
+          type="button"
+          disabled={!searchTerm.trim()}
+        >
+          Search
+        </button>
+      </form>
     );
 }
 
 export default SearchBar;
+
